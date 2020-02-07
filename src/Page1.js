d0fb86f245029c2712678a4c0080a80182be7d5a
kim young jo <kimyoungjo@kimui-MacBook-Pro.local>
2020-02-07 18:53:01 +0900

    배포완료

---
+++ src/Page1.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Main.css';
+import ContentFont from './ContentFont';
+import Button from './Button';
+import { Link } from 'react-router-dom';
+
+class Page1 extends React.Component {
+    render() {
+        return (
+            <div className="page row-div">
+                <div className="column-div width-90">
+                    <ContentFont text="Front-End Developer 김영조의 포트폴리오 입니다." />
+                    <div className="row-div width-100">
+                        <Button text="Github" url="https://github.com/yoyounn18" />
+                        <Button text="Blog" url="https://blex.kr/@yoyounn18" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Page1;
